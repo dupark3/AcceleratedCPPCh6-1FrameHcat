3ace6d3dcc026e682380ccfc137727396694487f
Du Park <dupark3@gmail.com>
2018-02-22 13:10:58 -0500

    Main function outline for input and function calls

--- main.cpp
+++ main.cpp
@@ -3,13 +3,24 @@
 #include <vector>
 
 int main() {
-    std::cout << "Input lines to frame: " << std::endl;
-    std::vector<std::string> lines;
+    std::cout << "Input first set of lines to frame: " << std::endl;
+    std::vector<std::string> firstPicture;
     std::string s;
-
-    // store original picture in vector
+    // Store first input in vector
     while(getline(std::cin, s))
-        lines.push_back(s);
+        firstPicture.push_back(s);
+
+    // Pass first picture to frame function, which will add asterisks around with padding, left-aligned
+    frame(firstPicture);
+
+    // Store second input in vector
+    std::cout << "Input second set of lines to horizontally concatenate: " << std::endl;
+    std::vector<std::string> secondPicture;
+    while(getline(std::cin, s))
+        secondPicture(push_back(s));
+
+    // Pass first and second pictures to hcat function, which will print both pictures next to each other.
+    hcat(firstPicture, secondPicture);
 
     return 0;
 }
