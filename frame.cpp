acd9cf6916060abfa76192a9d9b042278b69011e
Du Park <dupark3@gmail.com>
2018-02-22 14:03:42 -0500

    Working version of Frame

--- frame.cpp
+++ frame.cpp
@@ -1,23 +1,46 @@
+#include <algorithm> // max
+#include <iostream>
+#include <iterator> // distance
 #include <string>
 #include <vector>
 
 #include "frame.h"
 
 typedef std::string::iterator string_iter;
-typedef std::vector<std::string>::iterator vec_iter;
+typedef std::vector<std::string>::const_iterator vec_iter;
 typedef std::string::size_type string_size;
 
 // Uses iterators to find the longest string in a vector.
 string_size width(const std::vector<std::string>& vec){
     string_size maxLen = 0;
-    while (vec_iter i = vec.begin(); i != vec.end(); ++i){
-        maxLen = std::max(maxLen, std::distance(vec[i].begin(), vec[i].end()));
+
+    for(vec_iter i = vec.begin(); i != vec.end(); ++i){
+        maxLen = std::max(maxLen, i->size());
     }
+
+    return maxLen;
 }
 
-std::vector<std::string> frame(const std:;vector<std::string>& original){
+std::vector<std::string> frame(const std::vector<std::string>& original){
     std::vector<std::string> framed;
     string_size maxLen = width(original);
+
+    std::string border = std::string(maxLen + 4, '*');
+
+    // Add top border of asterisks
+    framed.push_back(border);
+
+    // Add body of frame
+    for(vec_iter i = original.begin(); i != original.end(); ++i){
+        std::string line = "* " +
+                           *i +
+                           (std::string(maxLen - i->size(), ' ')) +
+                           " *";
+        framed.push_back(line);
+    }
+
+    // Add bottom border of asterisks
+    framed.push_back(border);
 
     return framed;
 }
--- main.cpp
+++ main.cpp
@@ -2,7 +2,9 @@
 #include <string>
 #include <vector>
 
-typedef std::vector<std::string>::size_type vec_size;
+#include "frame.h"
+
+typedef std::vector<std::string>::const_iterator vec_iter;
 
 int main() {
     std::cout << "Input first set of lines to frame: " << std::endl;
@@ -14,19 +16,22 @@ int main() {
 
     // Pass first picture to frame function, which will add asterisks around with padding, left-aligned
     std::vector<std::string> framedFirst = frame(firstPicture);
+    // std::cout << framedFirst[0] << std::endl;
+    //std::cout << framedFirst[1] << std::endl;
+    //std::cout << framedFirst[2] << std::endl;
+    //std::cout << framedFirst[3] << std::endl;
 
-    while(vec_size i = 0; i != framedFirst.size(); ++i)
-        std::cout << framedFirst[i] << std::endl;
-
+    for(vec_iter i = framedFirst.begin(); i != framedFirst.end(); ++i)
+        std::cout << *i << std::endl;
 
     // Store second input in vector
     std::cout << "Input second set of lines to horizontally concatenate: " << std::endl;
     std::vector<std::string> secondPicture;
     while(getline(std::cin, s))
-        secondPicture(push_back(s));
+        secondPicture.push_back(s);
 
     // Pass first and second pictures to hcat function, which will print both pictures next to each other.
-    hcat(firstPicture, secondPicture);
+    // hcat(firstPicture, secondPicture);
 
     return 0;
 }
